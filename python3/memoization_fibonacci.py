5701e771ce26edea225c939c30304479d85171a7
ramaryup <ramaryup>
2017-10-16 01:11:39 +0200

    memoization Python Fibonacci - fix conversion of input and manage exception on input error

--- python3/memoization_fibonacci.py
+++ python3/memoization_fibonacci.py
@@ -1,12 +1,15 @@
+'''
+ Testing memoization using lru_cache against Fibonacci function
+'''
+
 import sys
 
-# Testing memoization using lru_cache
 from functools import lru_cache
 
 @lru_cache()
 def fibonacci(n):
     if type(n) != int:
-        raise TypeError("n must be positive integer")
+        raise TypeError("n must be positive integer . Type is", type(n))
     elif n < 1:
         raise ValueError("n must be > 0")
     elif n == 1 or n == 2:
@@ -17,11 +20,20 @@ def fibonacci(n):
 def test_fibonacci():
     print("Starting Fibonacci program")
     print("Input an integer : ")
-    number = input()
+    
+    text = input().strip()
+    try:
+        number = int(text)
+    except:   
+        print("Exception occured when converting input ExceptionType:{} Description:{}".format( sys.exc_info()[0], sys.exc_info()[1]))         
+        return
+
     try:
         result = fibonacci(number)
     except:
         print("Exception occured ExceptionType:{} Description:{}".format( sys.exc_info()[0], sys.exc_info()[1]))
+    else:
+        print("Fibonacci({})={}".format(number, result))
 
 if __name__ == "__main__" :
     test_fibonacci()
